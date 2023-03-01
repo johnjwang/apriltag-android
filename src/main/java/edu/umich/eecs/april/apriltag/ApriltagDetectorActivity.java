a784429c12c26afbb49477898c48924f71435822
John Wang <johnjwang1@gmail.com>
2023-03-01 02:52:46 -0800

    Render tag detections

--- src/main/java/edu/umich/eecs/april/apriltag/ApriltagDetectorActivity.java
+++ src/main/java/edu/umich/eecs/april/apriltag/ApriltagDetectorActivity.java
@@ -4,21 +4,18 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.TextureView;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
@@ -30,6 +27,19 @@ import androidx.camera.view.transform.ImageProxyTransformFactory;
 import androidx.camera.view.transform.OutputTransform;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -51,6 +61,7 @@ public class ApriltagDetectorActivity extends AppCompatActivity {
 
     private ExecutorService detectorExecutor;
     private TextureView tagView;
+    private OutputTransform previewViewTransform;
 
 
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 77;
@@ -70,6 +81,87 @@ public class ApriltagDetectorActivity extends AppCompatActivity {
         }
     }
 
+    private void renderDetection(ApriltagDetection detection, Canvas canvas,
+                                 CoordinateTransform coordinateTransform) {
+        Paint fillPaint = new Paint();
+        fillPaint.setColor(Color.GREEN);
+        fillPaint.setAlpha(128);
+        fillPaint.setStyle(Paint.Style.FILL);
+
+        Paint borderPaint = new Paint();
+        final int[] borderColors = new int[]{Color.GREEN, Color.WHITE, Color.WHITE, Color.RED};
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(10);
+
+        double[] points = detection.p;
+        if (points == null || points.length != 8) {
+            Log.w(TAG, "invalid detection coordinates");
+            return;
+        }
+
+        // Convert detection points to canvas points
+        float[] xPointsCanvas = new float[4];
+        float[] yPointsCanvas = new float[4];
+        for (int i = 0; i < 4; i++) {
+            PointF point = new PointF((float)points[2*i], (float)points[2*i + 1]);
+            coordinateTransform.mapPoint(point);
+            xPointsCanvas[i] = point.x;
+            yPointsCanvas[i] = point.y;
+        }
+
+        // Render filled outline of detections
+        Path fillPath = new Path();
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                fillPath.moveTo(xPointsCanvas[i], yPointsCanvas[i]);
+            } else {
+                fillPath.lineTo(xPointsCanvas[i], yPointsCanvas[i]);
+            }
+        }
+        fillPath.close();
+        canvas.drawPath(fillPath, fillPaint);
+
+        // Render stroke outline of detections
+        int colorIndex = 0;
+        for (int i = 0; i < 4; i++) {
+            Path borderPath = new Path();
+            borderPaint.setColor(borderColors[colorIndex++ % borderColors.length]);
+
+            borderPath.moveTo(xPointsCanvas[i], yPointsCanvas[i]);
+            borderPath.lineTo(xPointsCanvas[(i + 1) % 4], yPointsCanvas[(i + 1) % 4]);
+            canvas.drawPath(borderPath, borderPaint);
+        }
+
+        // Render tag ID in the center of the detection box
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(100);
+        String tagId = String.valueOf(detection.id);
+        float textWidth = textPaint.measureText(tagId);
+        float textHeight = textPaint.getFontMetrics().descent - textPaint.getFontMetrics().ascent;
+        PointF textCenter = new PointF((float)detection.c[0], (float)detection.c[1]);
+        coordinateTransform.mapPoint(textCenter);
+        float textX = (float)(textCenter.x - textWidth / 2);
+        float textY = (float)(textCenter.y + textHeight / 2 - textPaint.getFontMetrics().descent);
+        canvas.drawText(tagId, textX, textY, textPaint);
+    }
+
+    private void renderDetections(ArrayList<ApriltagDetection> detections, CoordinateTransform coordinateTransform) {
+        Canvas canvas = tagView.lockCanvas();
+        try {
+            canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+            for (ApriltagDetection detection : detections) {
+                renderDetection(detection, canvas, coordinateTransform);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error rendering detections: " + e.getMessage());
+        } finally {
+            if (canvas != null) {
+                tagView.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
     private void startCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
                 ProcessCameraProvider.getInstance(this);
@@ -84,13 +176,12 @@ public class ApriltagDetectorActivity extends AppCompatActivity {
             CameraSelector cameraSelector = new CameraSelector.Builder()
                     .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
 
-            // Configure preview surface
+            // Configure preview use case
             Preview preview = new Preview.Builder().build();
             PreviewView previewView = findViewById(R.id.viewFinder);
             preview.setSurfaceProvider(previewView.getSurfaceProvider());
-            OutputTransform target = previewView.getOutputTransform();
 
-            // Configure detector
+            // Configure image analysis use case
             // Default image size is 640x480. This seems to be fast, suggesting the downsampling
             // is hardware-accelerated.
             ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
@@ -110,8 +201,19 @@ public class ApriltagDetectorActivity extends AppCompatActivity {
                     Log.i(TAG, String.format("Image size %dx%d, %d tags detected in %d ms",
                             image.getWidth(), image.getHeight(), detections.size(), end - start));
 
-                    //OutputTransform source = new ImageProxyTransformFactory().getOutputTransform(image);
-                    //CoordinateTransform coordinateTransform = new CoordinateTransform(source, target);
+                    OutputTransform source =
+                            new ImageProxyTransformFactory().getOutputTransform(image);
+
+                    if (previewViewTransform != null) {
+                        CoordinateTransform coordinateTransform =
+                                new CoordinateTransform(source, previewViewTransform);
+                        renderDetections(detections, coordinateTransform);
+                    } else if (previewView.isLaidOut()) {
+                        // TODO(john): There must be a cleaner way of doing this...
+                        previewView.post(() -> {
+                            previewViewTransform = previewView.getOutputTransform();
+                        });
+                    }
 
                     image.close();
                 }
